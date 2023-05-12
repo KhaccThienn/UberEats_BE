c57c1924dd2a11a6314c48c1e285b3966fbbb793
Nguyễn Thị Kim Yến <yenn0750@gmail.com>
2023-05-12 10:46:00 +0700

    T2BE05 Feat Create API for order details

--- src/order_details/order_details.module.ts
+++ src/order_details/order_details.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { OrderDetailsService } from './order_details.service';
 import { OrderDetailsController } from './order_details.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderDeatailsEntity } from './entity/order_details.entity';
+import { RestaurantModule } from 'src/restaurant/restaurant.module';
+import { OrderModule } from 'src/order/order.module';
+import { ProductModule } from 'src/product/product.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([OrderDeatailsEntity]),
+    forwardRef(()=> RestaurantModule),
+    forwardRef(()=> OrderModule),
+    forwardRef(()=> ProductModule)
+  ],
   controllers: [OrderDetailsController],
-  providers: [OrderDetailsService]
+  providers: [OrderDetailsService],
+  exports: [TypeOrmModule]
 })
 export class OrderDetailsModule {}
