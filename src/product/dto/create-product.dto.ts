d967437bb0b42fcc8b87a6f116693f4fb92da04e
Nguyễn Thị Kim Yến <yenn0750@gmail.com>
2023-05-12 09:07:14 +0700

    Feat: Update Validator

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,25 +1,32 @@
-import { Type } from "class-transformer";
+import { Expose, Type } from "class-transformer";
 import { IsNotEmpty,IsNumber,Min,MinLength } from "class-validator";
 
 export class CreateProductDTO {
     @IsNotEmpty()
     @MinLength(3)
+    @Expose()
     name:string;
 
+    @IsNotEmpty()
+    @Expose()
     image: string;
 
     @IsNumber()
     @Min(1)
     @Type(()=> Number)
+    @Expose()
     price: number;
 
     @Min(0)
     @Type(()=> Number)
+    @Expose()
     sale_price: number;
 
     @IsNotEmpty()
+    @Expose()
     status: number;
     
     @IsNotEmpty()
+    @Expose()
     description: string;
 };
--- src/restaurant/dto/update-restaurant.dto.ts
+++ src/restaurant/dto/update-restaurant.dto.ts
@@ -1,9 +1,25 @@
+import { Expose } from 'class-transformer';
+import { IsNotEmpty,MinLength, IsString, IsEmail } from 'class-validator';
+
 export class UpdateRestaurantDTO {
+    @IsNotEmpty()
+    @MinLength(3)
+    @IsString()
+    @Expose()
     name: string;
-
+  
+    @IsNotEmpty()
+    @IsString()
+    @Expose()
     address: string;
-
+  
+    @IsNotEmpty()
+    @IsString()
+    @IsEmail()
+    @Expose()
     email: string;
-
+  
+    @IsNotEmpty()
+    @Expose()
     phone: string;
 };
